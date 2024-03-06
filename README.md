7ac728c1439ce68efc6688046039bf290e75ef86
iitenkida7 <takeshi.ishii@crossrink.co.jp>
2024-03-06 23:44:35 +0900

    First

---
+++ README.md
@@ -0,0 +1,17 @@
+# docker_laravel10_skeleton
+* Docker x Laravel10 の 一発環境構築
+
+## Install
+
+* git clone 不要 以下のコマンドでインストール できます。
+* 今の所 Macを 想定。
+
+```
+/bin/bash -ec "$(curl -fsSL https://raw.githubusercontent.com/iitenkida7/docker_laravel10_skeleton/main/install.sh)"
+```
+
+* ブラウザでSSL証明書エラーでアクセスできない場合は、以下のコマンドを実行 （パスは適宜変更）
+
+```
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain project/docker/nginx/ssl/server.crt
+```
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -xue
+
+CURRENT=$(pwd)
+INSTALL_DIR=${1:-${CURRENT}/project}
+
+if [ -d "${INSTALL_DIR}" ]; then
+  echo "Error: Already exists directory" 1>&2
+  exit 1;
+else
+  mkdir -p "${INSTALL_DIR}"
+fi
+
+cd ${INSTALL_DIR}
+
+git clone git@github.com:iitenkida7/docker_laravel11_skeleton.git .
+
+#Build docker-compose.yml
+sed -i "s/__UID__/$(id -u)/g" docker-compose.yml
+sed -i "s/__GID__/$(id -g)/g" docker-compose.yml
+
+#Build container
+docker-compose build #--pull --no-cache
+
+#Install laravel
+mkdir laravel
+docker-compose run --rm laravel composer create-project --prefer-dist laravel/laravel laravel-dev dev-master
+mv laravel laravel_ && mv laravel_/laravel .
+docker-compose up -d
+
+#Clean
+rm -rf .git install.sh laravel_ || true
+
+echo "Let's access!"
+echo "https://localhost/"
